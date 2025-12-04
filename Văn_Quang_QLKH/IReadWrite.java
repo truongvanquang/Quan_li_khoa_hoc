36fc0bf3b060a05f4e3086c89dee67823577c0a7
truongvanquang <truongvanquang662@gmail.com>
2025-12-04 10:17:41 +0700

    Delete Văn_Quang_QLKH directory

--- Văn_Quang_QLKH/IReadWrite.java
+++
@@ -1,9 +0,0 @@
-package javaapplication31;
-
-public interface IReadWrite {
-    String fileName = "khoahoc.txt";
-
-    void readData();
-    void writeData();
-    void exportCSV();
-}
--- Văn_Quang_QLKH/IThongKe.java
+++
@@ -1,6 +0,0 @@
-package javaapplication31;
-
-public interface IThongKe {
-    double thongKeTheoGV(String gv);
-    int thongKeTheoThang(int thang);
-}
